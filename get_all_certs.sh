b503d37f77b2a6be076bc0d8d79d7cec8cd1f748
easrng <me@easrng.net>
2022-11-06 17:15:39 +0000

    a

--- get_all_certs.sh
+++ get_all_certs.sh
@@ -2,4 +2,6 @@
 ORIG_DIR="$(pwd)"
 cd -- "$(dirname -- "$(command -v -- "$0")")" || exit 1
 parallel ./check_server.sh <"${1:-./pub/servers.list}"
+cd pub
+git add sites && git commit -m 'update cert list' && git push -u origin main
 cd "$ORIG_DIR" || exit 1
